dc17aeca7eefbc96dd532ba62b0ecdcf49e9c193
Samuthe <ekatisamuel@gmail.com>
2025-02-26 02:12:08 +0300

    changed database

--- studentrecord/settings.py
+++ studentrecord/settings.py
@@ -88,15 +88,22 @@ WSGI_APPLICATION = "studentrecord.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+# DATABASES = {
+#     "default": {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'srs4',
+#         'USER':'root',
+#         'PASSWORD':'Hacs!1tack',
+#         'HOST':'127.0.0.1',
+#         'PORT':'3306',
+#     }
+# }
+
 DATABASES = {
-    "default": {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'srs4',
-        'USER':'root',
-        'PASSWORD':'Hacs!1tack',
-        'HOST':'127.0.0.1',
-        'PORT':'3306',
-    }
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
 }
 
 
